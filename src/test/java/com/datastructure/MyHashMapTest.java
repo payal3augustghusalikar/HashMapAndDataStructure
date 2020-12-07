63eb9413d0bc5ed1660b00bd811d4c7203be1e29
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-07 16:52:18 +0530

    Added : initiate the project and add welcome message

---
+++ src/test/java/com/datastructure/MyHashMapTest.java
@@ -0,0 +1,16 @@
+package com.datastructure;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MyHashMapTest {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        System.out.println("\n ******* welcome to Data structure ********");
+    }
+
+    @Test
+    public void givenASentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
+
+    }
+}
